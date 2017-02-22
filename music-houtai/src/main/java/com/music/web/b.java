a3342e6e5da1f030a881956612cc1e55e4479891
J C <J C>
2017-02-22 19:46:29 +0800

    陈键：。。。

---
+++ music-houtai/src/main/java/com/music/web/b.java
@@ -0,0 +1,11 @@
+package com.music.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/q")
+public class b {
+	public b() {
+		System.out.println("b");
+	}
+}
